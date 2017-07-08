6c9cc870abd3d90f3c096d38303175b7afc0d486
sarah <smile-wu@hotmail.com>
2017-07-08 15:36:17 +0800

    welcome page

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,7 +1,7 @@
 module FlashesHelper
   FLASH_CLASSES = { alert: "danger", notice: "success", warning: "warning"}.freeze
 
-  def flash_class
+  def flash_class(key)
     FLASH_CLASSES.fetch key.to_sym, key
   end
 
