026af718ca3908acfff3cb88f836c4df80b91234
sarah <smile-wu@hotmail.com>
2017-07-13 17:47:43 +0800

    implement my groups and posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,12 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create]
-
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_post_and_check_permission, :only => [:edit, :update, :destroy]
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
+  end
+
+  def edit
   end
 
   def create
@@ -18,7 +21,29 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    if @post.update(post_params)
+      redirect_to account_posts_path
+      flash[:notice] = "Update success!"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to account_posts_path, alert: "Post deleted"
+  end
+
   private
+
+  def find_group_post_and_check_permission
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if current_user != @post.user
+      redirect_to account_posts_path, alert: "You have no permission!"
+    end
+  end
 
   def post_params
     params.require(:post).permit(:content)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'groups#index'
-  
+
   resources :groups do
     member do
       post :join
@@ -11,4 +11,10 @@ Rails.application.routes.draw do
 
     resources :posts
   end
+
+  namespace :account do
+    resources :groups
+    resources :posts
+  end
+
 end
